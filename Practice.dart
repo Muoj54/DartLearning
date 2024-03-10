0f07b98467cd5671ebcbd19a763e1f786fba0c1f
Muoj54 <160734738+Muoj54@users.noreply.github.com>
2024-03-10 11:19:05 +0300

    Dart Basics

---
+++ Practice.dart
@@ -0,0 +1,28 @@
+void main() {
+  // An Example of the int data type
+  int age = 25;
+  print('Age: $age');
+
+  // An example of the double data type
+  double height = 5.9;
+  print('Height: $height feet');
+
+  //An example of the String data type
+  String name = 'John';
+  print('Name: $name');
+
+  // An example of the List data type
+  List<String> fruits = ['Apple', 'Banana', 'Orange'];
+  print('Fruits: $fruits');
+
+  // An example of the Map data type
+  Map<String, dynamic> person = {
+    'name': 'Alice',
+    'age': 30,
+    'height': 5.6,
+    'fruits': ['Grapes', 'Kiwi']
+  };
+  print('Person Info: $person');
+
+  
+}
